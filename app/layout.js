20083680d6f13aa03f5098996df7977400b4a4a5
지훈한 <hoony@jihunhan-ui-MacBookAir.local>
2025-12-04 15:34:35 +0900

    title, description 변경

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,8 @@ import Script from 'next/script'
 import './globals.css'
 
 export const metadata = {
-  title: '예체능 입시생을 위한 겨울방학 전략 자료집 | 대치동 컨설턴트',
-  description: '대치동 예체능 재수 종합 학원 학습 관리 컨설턴트가 10년의 경험을 녹여낸 겨울방학 학습 전략법을 무료로 받아보세요.',
+  title: '예체능 입시생을 위한 최상의 학습 관리',
+  description: '예체능 입시생을 위한 최상의 학습 관리를 ST-예인과 함께하세요!',
 }
 
 export default function RootLayout({ children }) {
